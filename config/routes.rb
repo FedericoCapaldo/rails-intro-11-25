8fc763afac2195d2f450d27c2d0ebc47e92a1709
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-16 11:56:07 +0000

    added home route for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get "/", to: "pages#home"
 
   get "about", to: "pages#about"
 end
