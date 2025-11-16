99baf09af10e97f83f8bc33764a406ea43357591
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-16 11:56:53 +0000

    using root instead of get homepage in the route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  get "/", to: "pages#home"
+  root to: "pages#home"
 
   get "about", to: "pages#about"
 end
