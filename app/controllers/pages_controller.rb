54b77fabec25661c270376c2acab56fae7b3a694
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-16 12:38:57 +0000

    using params to actully filter on the member param

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,12 @@ class PagesController < ApplicationController
 
   def contact
     @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+
+    if params[:member]
+      @members = @members.select do |member|
+        member.start_with? params[:member].downcase
+      end
+    end
   end
 
   def about
